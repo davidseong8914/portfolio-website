9b1cc06f6ba531cf780dd63afcb8c88258162cde
Seungje <davidseong8914@gmail.com>
2024-06-26 23:40:05 +0900

    general update; version 2.0

--- script.js
+++ script.js
@@ -1,28 +1,58 @@
-let menuIcon = document.querySelector('#menu0icon');
+let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
-
 window.onscroll = () => {
-    sections.forEach(sec =>{
+    sections.forEach(sec => {
         let top = window.scrollY;
         let offset = sec.offsetTop - 150;
-        let height = sec.offesetHeight;
+        let height = sec.offsetHeight; // Corrected typo here
         let id = sec.getAttribute('id');
 
-        if(top >= offset && top < offset + height){
-            navLinks.forEach(links =>{
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
                 links.classList.remove('active');
-                document.querySelector('header nav a [href*=' + id + ' ]').classList.add('active')
-            })
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active') // Corrected the selector syntax
+            });
         }
-    })
-}
-
-
+    });
+};
 
 menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
-}
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+    const projectItems = document.querySelectorAll('.project-item');
+
+    projectItems.forEach(item => {
+        item.addEventListener('click', () => {
+            const projectId = item.id;
+            window.location.href = `projects/${projectId}.html`;
+        });
+    });
+
+    // Fun fact functionality
+    const funFacts = [
+        "I have traveled to 19 countries.",
+        "Yo hablo español.",
+        "I once did a 90 hour Greyhound trip from California to Virginia",
+        "I'm a ceramics master.",
+        "Favorite Book: Norwegian Wood by Haruki Murakami",
+        "Weather is great today",
+        "More to come...",
+        "More to come...",
+        "More to come...",
+    ];
+
+    const funFactBtn = document.getElementById("fun-fact-btn");
+    const funFactContainer = document.getElementById("fun-fact-container");
+
+    funFactBtn.addEventListener("click", function () {
+        const randomIndex = Math.floor(Math.random() * funFacts.length);
+        const randomFact = funFacts[randomIndex];
+        funFactContainer.innerText = randomFact;
+    });
+});
